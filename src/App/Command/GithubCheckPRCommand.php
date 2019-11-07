ac32ffe647e6b38504772804cb816d296afc668b
Progi1984 <franck.lefevre@prestashop.com>
2019-11-07 17:09:33 +0100

    Refactoring

--- src/App/Command/GithubCheckPRCommand.php
+++ src/App/Command/GithubCheckPRCommand.php
@@ -70,179 +70,43 @@ class GithubCheckPRCommand extends Command
         $date->sub(new DateInterval('P1D'));
 
         $mergedPullRequests = $this->client->api('search')->issues('repo:PrestaShop/PrestaShop is:pr is:merged merged:>'.$date->format('Y-m-d'));
-
-        $rows = [];
-        foreach($mergedPullRequests['items'] as $pullRequest) {
-            $linkedIssue = $this->getIssue($output, $pullRequest);
-            
-            $rows[] = [
-                '<href='.$pullRequest['html_url'].'>#'.$pullRequest['number'].'</>',
-                $pullRequest['created_at'],
-                $pullRequest['title'],
-                '<href='.$pullRequest['user']['html_url'].'>'.$pullRequest['user']['login'].'</>',
-                !empty($pullRequest['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>',
-                !is_null($linkedIssue) ? '<href='.$linkedIssue['html_url'].'>#'.$linkedIssue['number'].'</>' : '',
-                !is_null($linkedIssue) ? (!empty($linkedIssue['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>') : '',
-            ];
-        }
-        if (empty($rows)) {
-            return false;
-        }
-        if ($hasRows) {
-            $table->addRows([new TableSeparator()]);
-        }
-        $table->addRows([
-            [new TableCell('<fg=black;bg=white;options=bold> PR Merged </>', ['colspan' => 7])],
-            new TableSeparator(),
-            ['<info>#</info>', '<info>Created At</info>', '<info>Title</info>', '<info>Author</info>', '<info>Milestone</info>', '<info>Issue</info>', '<info>Milestone</info>'],
-            new TableSeparator(),
-        ]);
-        $table->addRows($rows);
-        return true;
+        return $this->checkPR($mergedPullRequests, $output, $table, $hasRows);
     }
 
     private function checkPRWaitingForMerge(InputInterface $input, OutputInterface $output, Table $table, bool $hasRows)
     {
         $mergedPullRequests = $this->client->api('search')->issues('repo:PrestaShop/PrestaShop is:open is:pr label:"QA ✔️"');
-
-        $rows = [];
-        foreach($mergedPullRequests['items'] as $pullRequest) {
-            $linkedIssue = $this->getIssue($output, $pullRequest);
-            
-            $rows[] = [
-                '<href='.$pullRequest['html_url'].'>#'.$pullRequest['number'].'</>',
-                $pullRequest['created_at'],
-                $pullRequest['title'],
-                '<href='.$pullRequest['user']['html_url'].'>'.$pullRequest['user']['login'].'</>',
-                !empty($pullRequest['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>',
-                !is_null($linkedIssue) ? '<href='.$linkedIssue['html_url'].'>#'.$linkedIssue['number'].'</>' : '',
-                !is_null($linkedIssue) ? (!empty($linkedIssue['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>') : '',
-            ];
-        }
-        if (empty($rows)) {
-            return false;
-        }
-        if ($hasRows) {
-            $table->addRows([new TableSeparator()]);
-        }
-        $table->addRows([
-            [new TableCell('<fg=black;bg=white;options=bold> PR Waiting for merge </>', ['colspan' => 7])],
-            new TableSeparator(),
-            ['<info>#</info>', '<info>Created At</info>','<info>Title</info>', '<info>Author</info>', '<info>Milestone</info>', '<info>Issue</info>', '<info>Milestone</info>'],
-            new TableSeparator(),
-        ]);
-        $table->addRows($rows);
-        return true;
+        return $this->checkPR($mergedPullRequests, $output, $table, $hasRows);
     }
 
     private function checkPRWaitingForQA(InputInterface $input, OutputInterface $output, Table $table, bool $hasRows)
     {
         $mergedPullRequests = $this->client->api('search')->issues('repo:PrestaShop/PrestaShop is:open is:pr label:"waiting for QA"');
-
-        $rows = [];
-        foreach($mergedPullRequests['items'] as $pullRequest) {
-            $linkedIssue = $this->getIssue($output, $pullRequest);
-            
-            $rows[] = [
-                '<href='.$pullRequest['html_url'].'>#'.$pullRequest['number'].'</>',
-                $pullRequest['created_at'],
-                $pullRequest['title'],
-                '<href='.$pullRequest['user']['html_url'].'>'.$pullRequest['user']['login'].'</>',
-                !empty($pullRequest['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>',
-                !is_null($linkedIssue) ? '<href='.$linkedIssue['html_url'].'>#'.$linkedIssue['number'].'</>' : '',
-                !is_null($linkedIssue) ? (!empty($linkedIssue['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>') : '',
-            ];
-        }
-        if (empty($rows)) {
-            return false;
-        }
-        if ($hasRows) {
-            $table->addRows([new TableSeparator()]);
-        }
-        $table->addRows([
-            [new TableCell('<fg=black;bg=white;options=bold> PR Waiting for QA </>', ['colspan' => 7])],
-            new TableSeparator(),
-            ['<info>#</info>', '<info>Created At</info>','<info>Title</info>', '<info>Author</info>', '<info>Milestone</info>', '<info>Issue</info>', '<info>Milestone</info>'],
-            new TableSeparator(),
-        ]);
-        $table->addRows($rows);
-        return true;
+        return $this->checkPR($mergedPullRequests, $output, $table, $hasRows);
     }
 
     private function checkPRWaitingForPM(InputInterface $input, OutputInterface $output, Table $table, bool $hasRows)
     {
         $mergedPullRequests = $this->client->api('search')->issues('repo:PrestaShop/PrestaShop is:open is:pr label:"waiting for PM"');
-
-        $rows = [];
-        foreach($mergedPullRequests['items'] as $pullRequest) {
-            $linkedIssue = $this->getIssue($output, $pullRequest);
-            
-            $rows[] = [
-                '<href='.$pullRequest['html_url'].'>#'.$pullRequest['number'].'</>',
-                $pullRequest['created_at'],
-                $pullRequest['title'],
-                '<href='.$pullRequest['user']['html_url'].'>'.$pullRequest['user']['login'].'</>',
-                !empty($pullRequest['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>',
-                !is_null($linkedIssue) ? '<href='.$linkedIssue['html_url'].'>#'.$linkedIssue['number'].'</>' : '',
-                !is_null($linkedIssue) ? (!empty($linkedIssue['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>') : '',
-            ];
-        }
-        if (empty($rows)) {
-            return false;
-        }
-        if ($hasRows) {
-            $table->addRows([new TableSeparator()]);
-        }
-        $table->addRows([
-            [new TableCell('<fg=black;bg=white;options=bold> PR Waiting for PM </>', ['colspan' => 7])],
-            new TableSeparator(),
-            ['<info>#</info>', '<info>Created At</info>','<info>Title</info>', '<info>Author</info>', '<info>Milestone</info>', '<info>Issue</info>', '<info>Milestone</info>'],
-            new TableSeparator(),
-        ]);
-        $table->addRows($rows);
-        return true;
+        return $this->checkPR($mergedPullRequests, $output, $table, $hasRows);
     }
 
     private function checkPRWaitingForUX(InputInterface $input, OutputInterface $output, Table $table, bool $hasRows)
     {
         $mergedPullRequests = $this->client->api('search')->issues('repo:PrestaShop/PrestaShop is:open is:pr label:"waiting for UX"');
-
-        $rows = [];
-        foreach($mergedPullRequests['items'] as $pullRequest) {
-            $linkedIssue = $this->getIssue($output, $pullRequest);
-            
-            $rows[] = [
-                '<href='.$pullRequest['html_url'].'>#'.$pullRequest['number'].'</>',
-                $pullRequest['created_at'],
-                $pullRequest['title'],
-                '<href='.$pullRequest['user']['html_url'].'>'.$pullRequest['user']['login'].'</>',
-                !empty($pullRequest['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>',
-                !is_null($linkedIssue) ? '<href='.$linkedIssue['html_url'].'>#'.$linkedIssue['number'].'</>' : '',
-                !is_null($linkedIssue) ? (!empty($linkedIssue['milestone']) ? '    <info>✓</info>' : '    <error>✗ </error>') : '',
-            ];
-        }
-        if (empty($rows)) {
-            return false;
-        }
-        if ($hasRows) {
-            $table->addRows([new TableSeparator()]);
-        }
-        $table->addRows([
-            [new TableCell('<fg=black;bg=white;options=bold> PR Waiting for UX </>', ['colspan' => 7])],
-            new TableSeparator(),
-            ['<info>#</info>', '<info>Created At</info>','<info>Title</info>', '<info>Author</info>', '<info>Milestone</info>', '<info>Issue</info>', '<info>Milestone</info>'],
-            new TableSeparator(),
-        ]);
-        $table->addRows($rows);
-        return true;
+        return $this->checkPR($mergedPullRequests, $output, $table, $hasRows);
     }
 
     private function checkPRWaitingForWording(InputInterface $input, OutputInterface $output, Table $table, bool $hasRows)
     {
         $mergedPullRequests = $this->client->api('search')->issues('repo:PrestaShop/PrestaShop is:open is:pr label:"waiting for Wording"');
+        return $this->checkPR($mergedPullRequests, $output, $table, $hasRows);
+    }
 
+    private function checkPR(array $returnSearch, OutputInterface $output, Table $table, bool $hasRows)
+    {
         $rows = [];
-        foreach($mergedPullRequests['items'] as $pullRequest) {
+        foreach($returnSearch['items'] as $pullRequest) {
             $linkedIssue = $this->getIssue($output, $pullRequest);
             
             $rows[] = [
@@ -256,7 +120,7 @@ class GithubCheckPRCommand extends Command
             ];
         }
         if (empty($rows)) {
-            return false;
+            return $hasRows;
         }
         if ($hasRows) {
             $table->addRows([new TableSeparator()]);
