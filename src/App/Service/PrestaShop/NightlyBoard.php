69d86a5da49b4b1e22817423021bf66991f565bc
Progi1984 <franck.lefevre@prestashop.com>
2025-06-19 11:25:32 +0200

    `slack:notifier` : Fixed number of reports for autoupgrade

--- src/App/Service/PrestaShop/NightlyBoard.php
+++ src/App/Service/PrestaShop/NightlyBoard.php
@@ -4,7 +4,7 @@ namespace Console\App\Service\PrestaShop;
 
 class NightlyBoard
 {
-    private $jsonResults = '';
+    private $jsonResults = [];
 
     /**
      * @param string $date
@@ -20,8 +20,7 @@ class NightlyBoard
         if (empty($json)) {
             return [];
         }
-        $json = json_decode($json, true);
-        $data = array_filter($json['reports'], function ($item) use ($date, $version, $campaign, $database) {
+        $data = array_filter($json, function ($item) use ($date, $version, $campaign, $database) {
             return $item['date'] === $date
                 && $item['version'] === $version
                 && $item['campaign'] === $campaign
@@ -43,8 +42,7 @@ class NightlyBoard
         if (empty($json)) {
             return [];
         }
-        $json = json_decode($json, true);
-        $data = array_filter($json['reports'], function ($item) use ($date, $campaign) {
+        $data = array_filter($json, function ($item) use ($date, $campaign) {
             return $item['date'] === $date && $item['campaign'] === $campaign;
         });
 
@@ -52,21 +50,36 @@ class NightlyBoard
     }
 
     /**
-     * @return string
+     * @return array
      */
-    protected function getReports(): string
+    protected function getReports(): array
     {
         if (!empty($this->jsonResults)) {
             return $this->jsonResults;
         }
 
-        $session = curl_init('https://api-nightly.prestashop-project.org/reports');
+        for ($page = 1; $page <= 10 ; $page++) {
+            $reportsPage = $this->getReportsPage($page);
+
+            $this->jsonResults = array_merge($this->jsonResults, $reportsPage);
+        }
+
+        return $this->jsonResults;
+    }
+
+    protected function getReportsPage(int $page = 1): array
+    {
+        $session = curl_init(sprintf(
+            'https://api-nightly.prestashop-project.org/reports?limit=100&page=%d',
+            $page
+        ));
         curl_setopt($session, CURLOPT_CUSTOMREQUEST, 'GET');
         curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($session, CURLOPT_SSL_VERIFYPEER, false);
-        $this->jsonResults = curl_exec($session);
+        $json = curl_exec($session);
         curl_close($session);
 
-        return $this->jsonResults;
+        $jsonArray = json_decode($json, true);
+        return !empty($jsonArray) && !empty($jsonArray['reports']) ? $jsonArray['reports'] : [];
     }
 }
